7baec8ce6996d75acdb0d2dca0f86daae95edba6
Zonaro <zonaro@outlook.com>
2022-05-05 11:44:22 -0300

    allow contentfiles

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -1,113 +1,163 @@
 "use strict";
 
+
+
+
+
 var lightboxes = [];
+const sass = new Sass();
 
 
-
-fetch('content.json', { mode: 'cors' })
-	.then((response) => {
-		response.json().then(function (json) {
-
-			json.forEach((item) => {
-				item.content = marked.parse(item.content ?? "");
-				item.aftercontent = marked.parse(item.aftercontent ?? "");
-				item.warning = marked.parse(item.warning ?? "");
-			});
-
-			Vue.createApp({
-				mounted: function () {
-					this.$nextTick(function () {
-
-						var sidebar = document.getElementById('docs-sidebar');
-						responsiveSidebar();
-
-						const spy = new Gumshoe('#docs-nav a', {
-							offset: 69 //sticky header height
-						});
-
-						/* ===== Smooth scrolling ====== */
-						/*  Note: You need to include smoothscroll.min.js (smooth scroll behavior polyfill) on the page to cover some browsers */
-						/* Ref: https://github.com/iamdustan/smoothscroll */
-
-						document.querySelectorAll('#docs-sidebar .scrollto').forEach((sidebarLink) => {
-
-							sidebarLink.addEventListener('click', (e) => {
-
-								e.preventDefault();
-
-								var target = sidebarLink.getAttribute("href").replace('#', '');
-
-								console.log(target);
-
-								document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
-						 
+async function getJson(url) {
+	return await fetch(url, { mode: 'cors' }).then(async (response) => await response.json().then(async (json) => await json));
+}
 
 
-								//Collapse sidebar after clicking
-								if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200) {
+async function getText(url) {
+	return await fetch(url, { mode: 'cors' }).then(async (response) => await response.text().then(async (txt) => await txt));
+}
 
-									sidebar.classList.remove('sidebar-visible');
-									sidebar.classList.add('sidebar-hidden');
-								}
+function parseHTML(html) {
+	var el = document.createElement('div');
+	el.innerHTML = html;
+	return el.childNodes;
+}
 
-							});
 
-						});
+async function getContent() {
+	var json = await getJson('content.json');
 
-						/* ====== SimpleLightbox Plugin ======= */
-						/*  Ref: https://github.com/andreknieriem/simplelightbox */
-						const boxes = document.querySelectorAll('[class*="simplelightbox-gallery-"]');
-						boxes.forEach(function (box) {
-							var classe = "." + box.className.split(" ")[0] + ' a'
-							console.log(classe);
-							lightboxes.push(new SimpleLightbox(classe, { /* options */ }));
-						});
+	if (json.title) {
+		json.title = marked.parse(json.title ?? "");
+		json.title = parseHTML(json.title)[0].innerHTML;
+	}
+	for (let index = 0; index < json.content.length; index++) {
+		let item = json.content[index];
 
-						document.getElementById('docs-sidebar-toggler').addEventListener('click', () => {
-							if (sidebar.classList.contains('sidebar-visible')) {
-								console.log('visible');
-								sidebar.classList.remove('sidebar-visible');
-								sidebar.classList.add('sidebar-hidden');
+		if (item.contentfile) {
+			item.content = await getText(item.contentfile);
+		}
 
-							} else {
-								console.log('hidden');
-								sidebar.classList.remove('sidebar-hidden');
-								sidebar.classList.add('sidebar-visible');
-							}
-						});
+		if (item.content)
+			item.content = marked.parse(item.content ?? "");
 
+
+		if (item.aftercontentfile) {
+			item.aftercontentfile = await getText(item.aftercontentfile);
+		}
+
+		if (item.aftercontent)
+			item.aftercontent = marked.parse(item.aftercontent ?? "");
+
+		if (item.warning)
+			item.warning = marked.parse(item.warning ?? "");
+
+		if (item.info)
+			item.info = marked.parse(item.info ?? "");
+
+		if (item.danger)
+			item.danger = marked.parse(item.danger ?? "");
+
+		console.log(item);
+	}
+
+	return json;
+}
+
+
+getContent().then((json) => {
+	Vue.createApp({
+		mounted: function () {
+			this.$nextTick(function () {
+
+				document.querySelectorAll(".search-form").forEach(function (x) {
+					x.addEventListener('submit', function (event) {
+						event.preventDefault();
+						window.find(x.children[0].value);
 					})
-				},
-				methods: {
-					fixId(id) {
-						id = `${id}`.split('.').join("-");
-						return id;
-					},
-					headerType(id) {
-						var i = id.split('.').length;
-						switch (i) {
-							case 1:
-								return "h1";
-							case 2:
-								return "h2";
-							default:
-								return "h5";
-						}
-					}
-				},
-				data() {
-					return { data: json };
-				}
-			}).mount('#app');
+				})
 
-		});
-	})
-	.then(function () {
-		console.log('Request successful');
-	})
-	.catch(function (error) {
-		console.log('Request failed', error)
-	});
+				var sidebar = document.getElementById('docs-sidebar');
+
+				responsiveSidebar();
+
+				const spy = new Gumshoe('#docs-nav a', {
+					offset: 69 //sticky header height
+				});
+
+				/* ===== Smooth scrolling ====== */
+				/*  Note: You need to include smoothscroll.min.js (smooth scroll behavior polyfill) on the page to cover some browsers */
+				/* Ref: https://github.com/iamdustan/smoothscroll */
+
+				document.querySelectorAll('#docs-sidebar .scrollto').forEach((sidebarLink) => {
+
+					sidebarLink.addEventListener('click', (e) => {
+
+						e.preventDefault();
+
+						var target = sidebarLink.getAttribute("href").replace('#', '');
+
+						document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
+
+						//Collapse sidebar after clicking
+						if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200) {
+							sidebar.classList.remove('sidebar-visible');
+							sidebar.classList.add('sidebar-hidden');
+						}
+
+					});
+
+				});
+
+				/* ====== SimpleLightbox Plugin ======= */
+				/*  Ref: https://github.com/andreknieriem/simplelightbox */
+				const boxes = document.querySelectorAll('[class*="simplelightbox-gallery-"]');
+				boxes.forEach(function (box) {
+					var classe = "." + box.className.split(" ")[0] + ' a'
+
+					lightboxes.push(new SimpleLightbox(classe, { /* options */ }));
+				});
+
+				document.getElementById('docs-sidebar-toggler').addEventListener('click', () => {
+					if (sidebar.classList.contains('sidebar-visible')) {
+
+						sidebar.classList.remove('sidebar-visible');
+						sidebar.classList.add('sidebar-hidden');
+
+					} else {
+
+						sidebar.classList.remove('sidebar-hidden');
+						sidebar.classList.add('sidebar-visible');
+					}
+				});
+
+			})
+		},
+		methods: {
+			fixId(id) {
+				id = `${id}`.split('.').join("-");
+				return id;
+			},
+			headerType(id) {
+				var i = id.split('.').length;
+				switch (i) {
+					case 1:
+						return "h1";
+					case 2:
+						return "h2";
+					default:
+						return "h5";
+				}
+			}
+		},
+		data() {
+			return { data: json };
+		}
+	}).mount('#app');
+
+});
+
+
 
 
 
@@ -116,10 +166,7 @@ window.onresize = function () {
 	responsiveSidebar();
 };
 
-window.search = function (form) {
-	window.find(form.children[0].value);
-	return false;
-};
+
 
 
 
